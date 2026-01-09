2b8b3cb1f5593e5f26904dad5922d93515305e7d
yonathanZel <zelnick@post.bgu.ac.il>
2026-01-09 12:07:00 +0000

    StompMessageProtocol started

--- server/src/main/java/bgu/spl/net/impl/stomp/StompFrame.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompFrame.java
@@ -8,77 +8,59 @@ public class StompFrame {
     private Map<String,String> headers = new HashMap<>();
     private String body;
 
-    public StompFrame(String commands)
-    {
-        this.command= commands;
+    public StompFrame(String commands) {
+        this.command = commands;
     }
 
-    public String getCommand()
-    {
+    public String getCommand() {
         return command;
-
     }
-    public void addHeader(String key,String value)
-    {
+    public void addHeader(String key, String value) {
         headers.put(key, value);
     }
-    public String getHeader(String key)
-    {
+    public String getHeader(String key) {
         return headers.get(key);
     }
-    public void setBody(String body)
-    {
-        this.body=body;
+    public void setBody(String body) {
+        this.body = body;
     }
-    public String getBody()
-    {
+    public String getBody() {
         return this.body;
     }
 
-    public static StompFrame fromString(String rawMessage)
-    {
+    public static StompFrame fromString(String rawMessage) {
+        StompFrame ret = null;
+        int i = 0;
 
-        StompFrame ret=null;
-        int i=0;
-
-        if(rawMessage==null || rawMessage.length()==0)
-        {
+        if (rawMessage == null || rawMessage.length() == 0) {
             return null;
         }
-        String[] lines = rawMessage.split("\n");   
 
-        //COMMAND
-        ret= new StompFrame(lines[i].trim());//create new StompFrame with Command string 
+        String[] lines = rawMessage.split("\n");
+
+        // COMMAND
+        ret = new StompFrame(lines[i].trim());
         i++;
-       
-       
-        //HEADERS -until end of lines or an empty line 
-        for(i=1; i<lines.length && !lines[i].isEmpty(); i++)
-        {
-            String[] parts = lines[i].split(":",2);
-            //Add new Header to frame 
-            if(parts.length ==2)
-            {
+
+        // HEADERS - until end of lines or an empty line
+        for (i = 1; i < lines.length && !lines[i].isEmpty(); i++) {
+            String[] parts = lines[i].split(":", 2);
+            // Add new Header to frame
+            if (parts.length == 2) {
                 ret.addHeader(parts[0].trim(), parts[1].trim());
-            }     
+            }
         }
 
-       
-       //BODY - build body until end of message
-       
-
-       
+        // BODY - build body until end of message
         StringBuilder bodyBuilder = new StringBuilder();
-     
-        //if we got to an empty line
-        if(i!= lines.length)
-        {   
-            i++;//contuing after empty line
-            for(;i<lines.length;i++)
-            {
+
+        // if we got to an empty line
+        if (i != lines.length) {
+            i++; // continuing after empty line
+            for (; i < lines.length; i++) {
                 bodyBuilder.append(lines[i]);
-                
-                //add new line
+
+                // add new line
                 if (i < lines.length - 1) {
                     bodyBuilder.append("\n");
                 }
@@ -87,34 +69,38 @@ public class StompFrame {
 
         ret.setBody(bodyBuilder.toString());
 
-
         return ret;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        
+
         // 1. Command
         sb.append(command).append("\n");
-        
+
         // 2. Headers
-        // (Map<String, String>)
         for (Map.Entry<String, String> header : headers.entrySet()) {
             sb.append(header.getKey())
-            .append(":")
-            .append(header.getValue())
-            .append("\n");
+                    .append(":")
+                    .append(header.getValue())
+                    .append("\n");
         }
-        
-        // 3. Empty Line _ SEPERATION
+
+        // 3. Empty Line - SEPARATION
         sb.append("\n");
-        
+
         // 4. Body
         if (body != null && !body.isEmpty()) {
             sb.append(body);
         }
-        
+
         return sb.toString();
+    }
+    public static StompFrame createErrorFrame(String header, String body) {
+        StompFrame errorFrame = new StompFrame("ERROR");
+        errorFrame.addHeader("message", header);
+        errorFrame.setBody(body);
+        return errorFrame;
     }
 }
--- server/src/main/java/bgu/spl/net/impl/stomp/StompMessagingProtocolImpl.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompMessagingProtocolImpl.java
@@ -5,28 +5,102 @@ import java.sql.Connection;
 import bgu.spl.net.api.StompMessagingProtocol;
 import bgu.spl.net.srv.Connections;
 
-public class StompMessagingProtocolImpl implements StompMessagingProtocol{
+public class StompMessagingProtocolImpl implements StompMessagingProtocol<StompFrame> {
     private int connectionId;
-    private Connections<String> connections;
+    private Connections<StompFrame> connections;
     private boolean shouldTerminate = false;
     private boolean isLoggedIn = false;
     private String currentUser = null;
 
     @Override
     public void start(int connectionId, Connections connections) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
+        this.connectionId = connectionId;
+        this.connections = connections;
     }
     @Override
-    public void process(Object message) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    public void process(StompFrame frame) {
+        String command = frame.getCommand();
+        // Check if user is logged in for commands other than CONNECT
+        if(!isLoggedIn && !command.equals("CONNECT")) {
+            //Create ERROR frame
+            StompFrame errorFrame = StompFrame.createErrorFrame("User not logged in", "You must log in before sending other commands.");
+            //Send error frame to client
+            connections.send(connectionId, errorFrame);
+        }
+        else{
+            switch (command) {
+                case "CONNECT":
+                    handleConnect(frame);
+                    break;
+                case "SEND":
+                    handleSend(frame);
+                    break;
+                case "SUBSCRIBE":
+                    handleSubscribe(frame);
+                    break;
+                case "UNSUBSCRIBE":
+                    handleUnsubscribe(frame);
+                    break;
+                case "DISCONNECT":
+                    handleDisconnect(frame);
+                    break;
+                default:
+                    //Create ERROR frame for unknown command
+                    StompFrame errorFrame = StompFrame.createErrorFrame("Unknown command", "The command '" + command + "' is not recognized.");
+                    //Send error frame to client
+                    connections.send(connectionId, errorFrame);
+                    shouldTerminate = true;
+                    break;
+        }
     }
 
+
+    }
+
+    private void handleDisconnect(StompFrame frame) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleDisconnect'");
+    }
+    private void handleUnsubscribe(StompFrame frame) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleUnsubscribe'");
+    }
+    private void handleSubscribe(StompFrame frame) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleSubscribe'");
+    }
+    private void handleSend(StompFrame frame) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'handleSend'");
+    }
+    private void handleConnect(StompFrame frame) {
+        if(isLoggedIn) {
+            //Create ERROR frame for already logged in user
+            StompFrame errorFrame = StompFrame.createErrorFrame("Already logged in", "You are already logged in.");
+            //Send error frame to client
+            connections.send(connectionId, errorFrame);
+            return;
+        }
+        String username = frame.getHeader("login");;
+        String passcode = frame.getHeader("passcode");
+        String acceptVersion = frame.getHeader("accept-version");
+
+        if (username == null || passcode == null) {
+            StompFrame errorFrame = StompFrame.createErrorFrame("Missing credentials", "Login or passcode header is missing.");
+            connections.send(connectionId, errorFrame);
+            this.shouldTerminate = true;
+            return;
+        }
+        // #TODO: Here you would normally check the username and passcode against a database or in-memory store
+        // #TODO: check database functions are enough for this assignment or need to be extended or connectionImpl.java
+        this.isLoggedIn = true;
+        StompFrame connectedFrame = new StompFrame("CONNECTED");
+        connectedFrame.addHeader("version", "1.2");
+        connections.send(connectionId, connectedFrame);
+    }
     @Override
     public boolean shouldTerminate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'shouldTerminate'");
+        return shouldTerminate;
     }
 
     
