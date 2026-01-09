c895be8dbd6dec01d87cc49f433c5facede7c12c
yonathanZel <zelnick@post.bgu.ac.il>
2026-01-09 11:02:57 +0000

    StompMessageEncoderDecoder implemented

---
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompMessageEncoderDecoder.java
@@ -0,0 +1,42 @@
+package bgu.spl.net.impl.stomp;
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+
+public class StompMessageEncoderDecoder implements MessageEncoderDecoder<StompFrame> {
+    private byte[] bytes = new byte[1024];
+    private int len = 0;
+    private static final int MAX_MESSAGE_SIZE = 1 << 20;
+    @Override
+    public StompFrame decodeNextByte(byte nextByte) {
+        if(nextByte == '\0'){
+            return popMessage();
+        }
+        pushByte(nextByte);
+        return null;
+    }
+
+    private void pushByte(byte nextByte) {
+        if (len >= MAX_MESSAGE_SIZE) {
+            throw new RuntimeException("Message size limit exceeded");
+        }
+        // if needed, resize the array
+        if (len >= bytes.length) {
+            //double the size of the array
+            bytes = java.util.Arrays.copyOf(bytes, len * 2);
+        }
+        bytes[len++] = nextByte;   
+    }
+    // when we get here we have a full message
+    private StompFrame popMessage() {
+        String message = new String(bytes,0,len, java.nio.charset.StandardCharsets.UTF_8);
+        len = 0;
+        return StompFrame.fromString(message);
+    }
+
+    @Override
+    public byte[] encode(StompFrame message) {
+        StringBuilder strMessage = new StringBuilder(message.toString());
+        strMessage.append('\0');
+        return strMessage.toString().getBytes(java.nio.charset.StandardCharsets.UTF_8);
+    }
+}
