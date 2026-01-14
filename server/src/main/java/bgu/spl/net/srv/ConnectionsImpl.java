7529dbb69a5c0f885712548bcc230de0e6866320
YannaiCo <yyaannaaii3@gmail.com>
2026-01-14 09:15:15 +0000

    Connection Impl added

--- server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -6,20 +6,37 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionsImpl<T> implements Connections<T> {
     private Map<Integer, ConnectionHandler<T>> connections = new ConcurrentHashMap<>();
-    private Map<String, Set<Integer>> topics = new ConcurrentHashMap<>();
+    //Map <"Topic" <ConnectionId, SubscriptionId>>
+    private Map<String,ConcurrentHashMap<Integer, String>> topics = new ConcurrentHashMap<>();
 
     @Override
     public boolean send(int connectionId, T msg){
+        ConnectionHandler<T> handler = connections.get(connectionId);
+        if(handler != null){
+            handler.send(msg);
+            return true;
+        }
         return false;
     }
 
     @Override
     public void send(String channel, T msg){
-
+        ConcurrentHashMap<Integer, String> subscribers = topics.get(channel);
+        if(subscribers != null){
+            Set<Integer> connectionIds = subscribers.keySet();
+            for(Integer id : connectionIds){
+                
+                send(id, msg);
+            }
+        }
     }
 
     @Override
     public void disconnect(int connectionId){
+        connections.remove(connectionId);//remove user from connections
+        for(ConcurrentHashMap<Integer, String> subscribers : topics.values()){ //remove user from all topics he subscribed to
+            subscribers.remove(connectionId);
+        }
 
     }
 
